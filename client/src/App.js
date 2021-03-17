179d9d5384f84cb4ad284a87d4c17cac3873272f
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-17 21:39:41 +0200

    finished basic

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokedecks</h1>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
